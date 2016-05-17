91c51ef6cf96c014021b22ea4da0fac1aa09ab2a
atdance <miccia2@mail.com>
2016-05-17 09:49:04 +0200

    added controller layer between Rest resource and DAO

--- src/main/java/com/movies/api/resource/RentalResource.java
+++ src/main/java/com/movies/api/resource/RentalResource.java
@@ -3,9 +3,6 @@
  */
 package com.movies.api.resource;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.ws.rs.core.MediaType;
@@ -17,10 +14,8 @@ import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.movies.model.Rental;
-import com.movies.model.RentalReturn;
+import com.movies.controller.Controller;
 import com.movies.model.error.ApiException;
-import com.movies.schema.RentalsDAO;
 
 import io.dropwizard.jackson.Jackson;
 
@@ -28,13 +23,14 @@ public class RentalResource implements Resource {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RentalResource.class);
 	private ObjectMapper aMAPPER = null;
-	private final RentalsDAO rentalsDAO;
+	private final Controller controller;
 	private static final AtomicInteger seq = new AtomicInteger();
 
-	public RentalResource(RentalsDAO dao) {
-		rentalsDAO = dao;
+	public RentalResource(Controller pController) {
+		controller = pController;
 
 		aMAPPER = Jackson.newObjectMapper();
+
 		aMAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 	}
 
@@ -49,49 +45,39 @@ public class RentalResource implements Resource {
 	@Override
 	public Response saveRental(String text) {
 		Response res = null;
-		Rental rental = null;
-		try {
-			rental = aMAPPER.readValue(text, Rental.class);
-		} catch (final IOException e) {
-			LOGGER.error(e.toString() + " " + e.getMessage() + " " + e.getCause(), e);
-		}
-		if (rental == null) {
+
+		String rentalID = null;
+
+		rentalID = controller.saveRental(text);
+
+		if (rentalID == null) {
 			res = Response.status(Status.INTERNAL_SERVER_ERROR).entity("rental is null")
 					.type(MediaType.APPLICATION_JSON).build();
 		} else {
-			final int id = rental.getId();
-
-			rentalsDAO.addByID(id, rental);
-
-			res = Response.ok(Integer.toString(id)).build();
+			res = Response.ok(rentalID).build();
 		}
 		return res;
 	}
 
 	@Override
 	public Response returnRental(final int pID, int pElapsedDays) {
-		RentalReturn ret = null;
+		int surcharge = 0;
+
 		try {
-
-			final Rental rental = rentalsDAO.findByID(pID);
-
-			ret = new RentalReturn(rental, pElapsedDays);
+			surcharge = controller.returnRental(pID, pElapsedDays);
 
 		} catch (final Exception e) {
 			LOGGER.error(e.toString(), e);
 			throw new ApiException("RENTAL_NOT_FOUND");
 		}
 
-		return Response.ok(ret.surCharge()).build();
+		return Response.ok(surcharge).build();
 	}
 
 	@Override
 	public Response getRentals() {
-		final Collection<Rental> rentals = new ArrayList<>();
-		for (final Rental rental : rentalsDAO.getAll()) {
-			rentals.add(rental);
-		}
-		return Response.ok(rentals).build();
+
+		return Response.ok(controller.getRentals()).build();
 	}
 
 }
